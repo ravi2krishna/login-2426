a66ddefeaba415ce7ba554d2e51fd307747ca930
ravi2krishna <ravi2krishna@gmail.com>
2024-10-01 12:08:58 +0530

    login frontend subnet

--- vpc-tf/vpc-resources.tf
+++ vpc-tf/vpc-resources.tf
@@ -8,3 +8,14 @@ resource "aws_vpc" "login-vpc" {
   }
 }
 
+# Subnet For Frontend
+resource "aws_subnet" "login-fe-sn" {
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "login-frontend-subnet"
+  }
+}
