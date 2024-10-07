9ddf477a9b93d5ab59aaa8fc898e10ebd63ab1ee
ravi2krishna <ravi2krishna@gmail.com>
2024-10-07 11:56:51 +0530

    NACL custom

--- vpc-tf/vpc-resources.tf
+++ vpc-tf/vpc-resources.tf
@@ -92,3 +92,30 @@ resource "aws_route_table_association" "login_db_asc" {
   subnet_id      = aws_subnet.login-db-sn.id
   route_table_id = aws_route_table.login_pvt_rt.id
 }
+
+# NACL for Custom Rules
+resource "aws_network_acl" "login_custom_nacl" {
+  vpc_id = aws_vpc.login-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "login-custom-nacl"
+  }
+}
